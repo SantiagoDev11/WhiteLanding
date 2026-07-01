20fa9d0f62b3188c21b687da5ade20d35bedcd41
gpo edit <mjmc@MJMC.COM>
2026-06-30 23:07:56 -0500

    Act4

--- white-landing/vite.config.js
+++ white-landing/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WhiteLanding/'
+  base: process.env.NODE_ENV == 'production'
+  ? '/'
+  : '/'
 })
